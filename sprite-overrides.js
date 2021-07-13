0100f5312e6f97c2312620c7997470abc71422cc
Johannes Emerich <johannes@dividat.com>
2021-07-13 09:52:38 +0200

    Add first skier animations

--- sprite-overrides.js
+++ sprite-overrides.js
@@ -1,8 +1,21 @@
 window.spriteOverrides = {
   skier: {
-    sizeMultiple: 0.1
+    sizeMultiple: 0.05,
+    parts: {
+      sEast: { frames: 3, fps: 7 },
+      jumping: { frames: 4, fps: 7 }
+    }
   },
   tallTree: {
+    sizeMultiple: 0.2
+  },
+  smallTree: {
     sizeMultiple: 0.08
+  },
+  rock: {
+    sizeMultiple: 0.2
+  },
+  signStart: {
+    sizeMultiple: 0.3
   }
 }
